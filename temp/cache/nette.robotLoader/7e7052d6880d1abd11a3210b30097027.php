0f81ef452572167d4b6fc561ab1c306a9b5add35
Matyáš Závora <matyaszavora@outlook.com>
2024-11-21 15:04:05 +0100

    Initial commit

---
+++ temp/cache/nette.robotLoader/7e7052d6880d1abd11a3210b30097027.php
@@ -0,0 +1,45 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/home/zavoram/nginx/kecal/app/Core/RouterFactory.php',
+      1 => 1730991161,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/home/zavoram/nginx/kecal/app/Bootstrap.php',
+      1 => 1730991161,
+    ),
+    'App\\UI\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/home/zavoram/nginx/kecal/app/UI/Accessory/LatteExtension.php',
+      1 => 1730991161,
+    ),
+    'App\\UI\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/home/zavoram/nginx/kecal/app/UI/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1730991161,
+    ),
+    'App\\UI\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/home/zavoram/nginx/kecal/app/UI/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1730991161,
+    ),
+    'App\\UI\\Home\\HomePresenter' => 
+    array (
+      0 => '/home/zavoram/nginx/kecal/app/UI/Home/HomePresenter.php',
+      1 => 1730991161,
+    ),
+  ),
+  1 => 
+  array (
+    'null' => 1,
+    'App\\UI\\Home\\HomeDefaultTemplate' => 1,
+    'App\\UI\\Home\\HomeTemplate' => 1,
+  ),
+  2 => 
+  array (
+  ),
+);

6c02b97c1f7fc230858147bd801f8e90b5eaba35
Matyáš Závora <matyaszavora@outlook.com>
2024-12-12 11:58:18 +0100

    Fixed coverage

--- app/Domain/Chatroom/ChatroomEntity.php
+++ app/Domain/Chatroom/ChatroomEntity.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Chatroom;
 
+use App\Domain\Message\MessageEntity;
 use App\Domain\User\User;
 use App\Model\Database\Entity\AbstractEntity;
 use DateTime;
@@ -15,85 +16,90 @@ use Doctrine\ORM\Mapping as ORM;
 class ChatroomEntity extends AbstractEntity
 {
 
-	/**
-	 * @ORM\Id
-	 * @ORM\GeneratedValue
-	 * @ORM\Column(type="integer")
-	 */
-	private int $id;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
 
-	/**
-	 * @ORM\ManyToOne(targetEntity="App\Domain\User\User")
-	 * @ORM\JoinColumn(nullable=false)
-	 */
-	private User $user1;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Domain\User\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private User $user1;
 
-	/**
-	 * @ORM\ManyToOne(targetEntity="App\Domain\User\User")
-	 * @ORM\JoinColumn(nullable=false)
-	 */
-	private User $user2;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Domain\User\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private User $user2;
 
-	/** @ORM\Column(type="integer", nullable=true) */
-	private ?int $lastMessageUserId = null;
+    /** @ORM\Column(type="integer", nullable=true) */
+    private ?int $lastMessageUserId = null;
 
-	/** @ORM\Column(type="datetime", nullable=true) */
-	private ?DateTime $lastMessageAt = null;
+    /** @ORM\Column(type="datetime", nullable=true) */
+    private ?DateTime $lastMessageAt = null;
 
-	public function __construct(User $user1, User $user2)
-	{
-		$this->user1 = $user1;
-		$this->user2 = $user2;
-	}
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Domain\Message\MessageEntity", inversedBy="chatroom")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private MessageEntity $message;
 
-	public function getId(): int
-	{
-		return $this->id;
-	}
+    public function __construct(User $user1, User $user2)
+    {
+        $this->user1 = $user1;
+        $this->user2 = $user2;
+    }
 
-	public function getUser1(): User
-	{
-		return $this->user1;
-	}
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
-	public function getUser2(): User
-	{
-		return $this->user2;
-	}
+    public function getUser1(): User
+    {
+        return $this->user1;
+    }
 
-	public function getLastMessageUserId(): ?int
-	{
-		return $this->lastMessageUserId;
-	}
+    public function getUser2(): User
+    {
+        return $this->user2;
+    }
 
-	public function getLastMessageAt(): ?DateTime
-	{
-		return $this->lastMessageAt;
-	}
+    public function getLastMessageUserId(): ?int
+    {
+        return $this->lastMessageUserId;
+    }
 
-	public function setLastMessageUserId(?int $lastMessageUserId): void
-	{
-		$this->lastMessageUserId = $lastMessageUserId;
-	}
+    public function getLastMessageAt(): ?DateTime
+    {
+        return $this->lastMessageAt;
+    }
 
-	public function setLastMessageAt(?DateTime $lastMessageAt): void
-	{
-		$this->lastMessageAt = $lastMessageAt;
-	}
+    public function setLastMessageUserId(?int $lastMessageUserId): void
+    {
+        $this->lastMessageUserId = $lastMessageUserId;
+    }
 
-	public function setLastMessageData(?int $lastMessageUserId, ?DateTime $lastMessageAt): void
-	{
-		$this->setLastMessageUserId($lastMessageUserId);
-		$this->setLastMessageAt($lastMessageAt);
-	}
+    public function setLastMessageAt(?DateTime $lastMessageAt): void
+    {
+        $this->lastMessageAt = $lastMessageAt;
+    }
 
-	public function getSecondUser(User $user): User
-	{
-		if ($this->user1 === $user) {
-			return $this->user2;
-		}
+    public function setLastMessageData(?int $lastMessageUserId, ?DateTime $lastMessageAt): void
+    {
+        $this->setLastMessageUserId($lastMessageUserId);
+        $this->setLastMessageAt($lastMessageAt);
+    }
 
-		return $this->user1;
-	}
+    public function getSecondUser(User $user): User
+    {
+        if ($this->user1 === $user) {
+            return $this->user2;
+        }
 
+        return $this->user1;
+    }
 }
